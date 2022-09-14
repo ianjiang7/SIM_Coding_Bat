643eb9cfde7673b151bd4dcb4dc9f492b49dfc78
Ian Jiang <73182358+ianjiang7@users.noreply.github.com>
2022-09-14 13:19:58 -0400

    Create warmup-1.py

---
+++ warmup-1.py
@@ -0,0 +1,45 @@
+def sleep_in(weekday, vacation):
+  return (not weekday) or (vacation)
+
+def monkey_trouble(a_smile, b_smile):
+  return a_smile == b_smile
+
+def sum_double(a, b):
+  sum = a + b
+  if a == b:
+    return sum * 2
+  else:
+    return sum
+  
+def diff21(n):
+  diff = abs(n - 21)
+  if n > 21:
+    return diff * 2
+  else:
+    return diff
+  
+def parrot_trouble(talking, hour):
+  return (talking) and ((hour < 7) or (hour > 20))
+
+def makes10(a, b):
+  sum = a + b
+  return (sum == 10) or (a == 10) or (b == 10)
+
+def makes10(a, b):
+  sum = a + b
+  return (sum == 10) or (a == 10) or (b == 10)
+
+def pos_neg(a, b, negative):
+  if negative:
+    return (a < 0 and b < 0)
+  else:
+    return (a < 0 and b > 0) or (a > 0 and b < 0)
+
+def not_string(str):
+  if str[0:3] == "not":
+    return str
+  else:
+    return "not " + str
+
+def missing_char(str, n):
+  return str[0:n] + str[n+1:len(str)]
