904735fa3590e085e5c02978d5e2139ea38e99cc
Ian Jiang <73182358+ianjiang7@users.noreply.github.com>
2022-09-21 12:57:55 -0400

    added test cases

--- warmup-1.py
+++ warmup-1.py
@@ -1,58 +1,78 @@
-#The parameter weekday is True if it is a weekday, and the parameter vacation is True if we are on vacation. We sleep in if it is not a weekday or we're on vacation. Return True if we sleep in.
 def sleep_in(weekday, vacation):
   return (not weekday) or (vacation)
+# sleep_in(False, False) → True
+# sleep_in(True, False) → False
+# sleep_in(False, True) → True
 
-
-#We have two monkeys, a and b, and the parameters a_smile and b_smile indicate if each is smiling. We are in trouble if they are both smiling or if neither of them is smiling. Return True if we are in trouble.
 def monkey_trouble(a_smile, b_smile):
   return a_smile == b_smile
+# monkey_trouble(True, True) → True
+# monkey_trouble(False, False) → True
+# monkey_trouble(True, False) → False
 
-#Given two int values, return their sum. Unless the two values are the same, then return double their sum.
 def sum_double(a, b):
   sum = a + b
   if a == b:
     return sum * 2
   else:
     return sum
+# sum_double(1, 2) → 3
+# sum_double(3, 2) → 5
+# sum_double(2, 2) → 8
 
-  
-#Given an int n, return the absolute difference between n and 21, except return double the absolute difference if n is over 21.
 def diff21(n):
   diff = abs(n - 21)
   if n > 21:
     return diff * 2
   else:
     return diff
-  
-  
-#We have a loud talking parrot. The "hour" parameter is the current hour time in the range 0..23. We are in trouble if the parrot is talking and the hour is before 7 or after 20. Return True if we are in trouble.
+# diff21(19) → 2
+# diff21(10) → 11
+# diff21(21) → 0  
+
 def parrot_trouble(talking, hour):
   return (talking) and ((hour < 7) or (hour > 20))
-
-#Given 2 ints, a and b, return True if one if them is 10 or if their sum is 10.
-def makes10(a, b):
-  sum = a + b
-  return (sum == 10) or (a == 10) or (b == 10)
-
+# parrot_trouble(True, 6) → True
+# parrot_trouble(True, 7) → False
+# parrot_trouble(False, 6) → False
 
 def makes10(a, b):
   sum = a + b
   return (sum == 10) or (a == 10) or (b == 10)
+# makes10(9, 10) → True
+# makes10(9, 9) → False
+# makes10(1, 9) → True
+
+def makes10(a, b):
+  sum = a + b
+  return (sum == 10) or (a == 10) or (b == 10)
+# near_hundred(93) → True
+# near_hundred(90) → True
+# near_hundred(89) → False
 
 def pos_neg(a, b, negative):
   if negative:
     return (a < 0 and b < 0)
   else:
     return (a < 0 and b > 0) or (a > 0 and b < 0)
+# pos_neg(1, -1, False) → True
+# pos_neg(-1, 1, False) → True
+# pos_neg(-4, -5, True) → True
 
 def not_string(str):
   if str[0:3] == "not":
     return str
   else:
     return "not " + str
-
+# not_string('candy') → 'not candy'
+# not_string('x') → 'not x'
+# not_string('not bad') → 'not bad'
+  
 def missing_char(str, n):
   return str[0:n] + str[n+1:len(str)]
+# missing_char('kitten', 1) → 'ktten'
+# missing_char('kitten', 0) → 'itten'
+# missing_char('kitten', 4) → 'kittn'
 
 def front_back(str):
   if len(str) == 1 or len(str) == 0: #return string if it is one or less chars
@@ -61,6 +81,12 @@ def front_back(str):
     return str[::-1]
   else:
     return str[-1] + str[1:len(str)-1] + str[0] #swap first and last chars
+# front_back('code') → 'eodc'
+# front_back('a') → 'a'
+# front_back('ab') → 'ba'
 
 def front3(str):
   return str[0:3]*3
+# front3('Java') → 'JavJavJav'
+# front3('Chocolate') → 'ChoChoCho'
+# front3('abc') → 'abcabcabc'
